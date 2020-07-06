ce9c6cf4e0de47c3db837293ad0bca7aa6e1a943
Hannah Corner <hannahcorner15@gmail.com>
2020-07-05 22:00:18 -0500

    Checkpoint 13: completed

---
+++ app.js
@@ -0,0 +1,43 @@
+
+function onReady() {
+  const ADD_TODO_FORM = document.getElementById('addToDoForm');
+  let toDos = [];
+
+  function renderTheUI(){
+    const TODO_LIST = document.getElementById('toDoList');
+    TODO_LIST.textContent = '';
+
+    toDos.forEach(function(toDo){
+      const NEW_LI = document.createElement('li');
+      const CHECKBOX = document.createElement('input');
+      CHECKBOX.type = "checkbox";
+
+      NEW_LI.textContent = toDo.title;
+      NEW_LI.appendChild(CHECKBOX);
+
+      TODO_LIST.appendChild(NEW_LI);
+    });
+  }
+
+  function createNewToDo(){
+    const NEW_TODO_TEXT = document.getElementById('newToDoText');
+    if(!NEW_TODO_TEXT.value){ return; }
+    toDos.push({
+      title: NEW_TODO_TEXT.value,
+      complete: false
+    });
+    NEW_TODO_TEXT.value = '';
+    renderTheUI();
+  }
+
+  ADD_TODO_FORM.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
+};
